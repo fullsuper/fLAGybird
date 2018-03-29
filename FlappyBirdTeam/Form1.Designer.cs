dc26346cad873485b723f46ebd190fbf62d114ea
Đồng <32479791+fullsuper@users.noreply.github.com>
2018-03-29 16:31:20 +0700

    First Blood

---
+++ FlappyBirdTeam/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace FlappyBirdTeam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.FlyingBird = new System.Windows.Forms.PictureBox();
+            this.pipedown1 = new System.Windows.Forms.PictureBox();
+            this.pipeup2 = new System.Windows.Forms.PictureBox();
+            this.pipedown2 = new System.Windows.Forms.PictureBox();
+            this.pipeup1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.FlyingBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FlyingBird
+            // 
+            this.FlyingBird.BackColor = System.Drawing.Color.Transparent;
+            this.FlyingBird.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("FlyingBird.BackgroundImage")));
+            this.FlyingBird.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.FlyingBird.Location = new System.Drawing.Point(40, 200);
+            this.FlyingBird.Name = "FlyingBird";
+            this.FlyingBird.Size = new System.Drawing.Size(50, 40);
+            this.FlyingBird.TabIndex = 0;
+            this.FlyingBird.TabStop = false;
+            // 
+            // pipedown1
+            // 
+            this.pipedown1.BackColor = System.Drawing.Color.Transparent;
+            this.pipedown1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pipedown1.BackgroundImage")));
+            this.pipedown1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pipedown1.Location = new System.Drawing.Point(325, 450);
+            this.pipedown1.Name = "pipedown1";
+            this.pipedown1.Size = new System.Drawing.Size(50, 500);
+            this.pipedown1.TabIndex = 1;
+            this.pipedown1.TabStop = false;
+            // 
+            // pipeup2
+            // 
+            this.pipeup2.BackColor = System.Drawing.Color.Transparent;
+            this.pipeup2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pipeup2.BackgroundImage")));
+            this.pipeup2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pipeup2.Location = new System.Drawing.Point(800, -300);
+            this.pipeup2.Name = "pipeup2";
+            this.pipeup2.Size = new System.Drawing.Size(50, 500);
+            this.pipeup2.TabIndex = 1;
+            this.pipeup2.TabStop = false;
+            // 
+            // pipedown2
+            // 
+            this.pipedown2.BackColor = System.Drawing.Color.Transparent;
+            this.pipedown2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pipedown2.BackgroundImage")));
+            this.pipedown2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pipedown2.Location = new System.Drawing.Point(800, 350);
+            this.pipedown2.Name = "pipedown2";
+            this.pipedown2.Size = new System.Drawing.Size(50, 500);
+            this.pipedown2.TabIndex = 1;
+            this.pipedown2.TabStop = false;
+            // 
+            // pipeup1
+            // 
+            this.pipeup1.BackColor = System.Drawing.Color.Transparent;
+            this.pipeup1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pipeup1.BackgroundImage")));
+            this.pipeup1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pipeup1.Location = new System.Drawing.Point(325, -200);
+            this.pipeup1.Name = "pipeup1";
+            this.pipeup1.Size = new System.Drawing.Size(50, 500);
+            this.pipeup1.TabIndex = 1;
+            this.pipeup1.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(214, 209);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(119, 63);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pipeup1);
+            this.Controls.Add(this.pipeup2);
+            this.Controls.Add(this.pipedown2);
+            this.Controls.Add(this.pipedown1);
+            this.Controls.Add(this.FlyingBird);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.FlyingBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipedown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeup1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox FlyingBird;
+        private System.Windows.Forms.PictureBox pipedown1;
+        private System.Windows.Forms.PictureBox pipeup2;
+        private System.Windows.Forms.PictureBox pipedown2;
+        private System.Windows.Forms.PictureBox pipeup1;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ FlappyBirdTeam/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBirdTeam
+{
+    public partial class Form1 : Form
+    {
+
+        #region  
+
+        int x_first = 300;
+        int x_second = 600;
+
+        int y_first = -200;
+        int y_second = -300;
+
+        int x_bird = 40;
+        int y_bird = 200;
+        Timer tmRunPipe,tmDropBird;
+
+        Random rd;
+        bool isFlying = true;
+
+        #endregion
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Space || e.KeyCode == Keys.Space)
+            {
+                y_bird -= 50;
+                FlyingBird.Location = new Point(x_bird, y_bird);
+                FlyingBird.BackgroundImage = Image.FromFile("E:\\VS\\FlappyBirdTeam\\FlappyBirdTeam\\Image\\dropbird2.png");
+                isFlying = true;
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //this.DoubleBuffered = true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+          //  btnStart.Hide();
+            btnStart.Dispose();
+
+            tmRunPipe = new Timer();
+            tmRunPipe.Start();
+            
+            tmRunPipe.Tick += TmRunPipe_Tick;
+            tmRunPipe.Interval = 10;
+
+            tmDropBird = new Timer();
+            tmDropBird.Tick += TmDropBird_Tick;
+            tmDropBird.Interval = 100;
+            tmDropBird.Start();
+        }
+
+        private void TmDropBird_Tick(object sender, EventArgs e)
+        {
+            y_bird+=10;
+            FlyingBird.Location = new Point(x_bird,y_bird);
+        }
+
+        private void TmRunPipe_Tick(object sender, EventArgs e)
+        {
+            x_first -= 2;
+            x_second -= 2;
+            pipeup1.Location = new Point(x_first, y_first);
+            pipeup2.Location = new Point(x_second, y_second );
+            pipedown1.Location = new Point(x_first,y_first +600);
+            pipedown2.Location = new Point(x_second, y_second+600);
+            if (isFlying)
+            {
+                FlyingBird.BackgroundImage = Image.FromFile("E:\\VS\\FlappyBirdTeam\\FlappyBirdTeam\\Image\\flybird2.png");
+                isFlying = false;
+            }
+            Reload();
+        }
+        public void Reload()
+        {
+            if (isGameOver())
+            {
+                tmRunPipe.Stop();
+                tmDropBird.Stop();
+                MessageBox.Show("Gem au vờ");
+            }
+            //random khe
+            if (x_first <= -50)
+            {
+                rd = new Random();
+                y_first = -500 + rd.Next(150,350);
+                x_first = 600;
+                pipeup1.Location = new Point(600, y_first);
+                pipedown1.Location = new Point(600, y_first + 600);
+            }
+            if (x_second <= -50)
+            {
+                rd = new Random();
+                y_second = -500 + rd.Next(150,350);
+                x_second = 600;
+                pipeup2.Location = new Point(600, y_second);
+                pipedown2.Location = new Point(600, y_second + 600);
+                
+            }
+            
+
+        }
+
+
+        public bool isGameOver()
+        {
+            if (x_first > x_bird && x_first < x_bird + 50)
+                if (y_bird < y_first + 500 || y_bird + 40 > y_first + 600)
+                    return true;
+            if (x_second > x_bird && x_second < x_bird + 50)
+                if (y_bird < y_second + 500 || y_bird + 40 > y_second + 600)
+                    return true;
+            if (y_bird > 420 || y_bird < 0)
+                return true;
+            return false;
+        }
+
+    }
+}
